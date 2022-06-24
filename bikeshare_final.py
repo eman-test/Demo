ba6017427accd776b19b37b65f893c32b0783c24
eman-test <104280830+eman-test@users.noreply.github.com>
2022-06-24 14:45:06 +0200

    Update bikeshare_final.py

--- bikeshare_final.py
+++ bikeshare_final.py
@@ -13,7 +13,7 @@ def get_filters():
     Returns:
         (str) city -  name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) day - name of the day of wek to filter by, or "all" to apply no day filter
     """
     print('Hello!! Let\'s explore some US bikeshare data!')
     city =   input("Please select the city you want to analyze data about (chicago, new york, washington):\n").lower()
